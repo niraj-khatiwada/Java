a6460e07a444fbef2a2aaede48c8b85476e2de8a
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-03-20 20:58:45 +0545

    string

---
+++ src/string.java
@@ -0,0 +1,18 @@
+// package src;
+
+// class Main {
+// public static void main(String[] args) {
+// String name = "Niraj";
+// String name2 = "Niraj";
+
+// System.out.printf("Length = %s \n", name.length());
+// System.out.printf("StartsWith = %s \n", name.startsWith("N"));
+// System.out.printf("EndsWith = %s \n", name.endsWith("j"));
+// System.out.printf("Index of = %s \n", name.indexOf("j"));
+// System.out.printf("Is equal? = %s %s \n", name == name2, name.equals(name2));
+// System.out.printf("Replace = %s \n", name.replace("Nir", "Sur"));
+// System.out.printf("Lowercase = %s \n", name.toLowerCase());
+// System.out.printf("Uppercase = %s \n", name.toUpperCase());
+// System.out.printf("Trim = %s \n", (" " + name).trim());
+// }
+// }
