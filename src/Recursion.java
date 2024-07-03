74083671388e783e9bf48f6336deb328cc8387f8
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-07-03 12:14:08 +0545

    Recursion

--- src/Recursion.java
+++ src/Recursion.java
@@ -5,7 +5,7 @@ class Recursion {
     }
 
     public static int factorial(int num) {
-        if (num == 1) {
+        if (num == 0) {
             return 1;
         }
         return num * factorial(num - 1);
