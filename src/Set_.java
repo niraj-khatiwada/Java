f86fec2d041071ef623cd79589bd8a67524837ec
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-08-18 16:47:12 +0545

    Tree Set

--- src/Set_.java
+++ src/Set_.java
@@ -4,6 +4,27 @@ import java.util.*;
 class Set {
 
     public static void main(String[] args) {
+        // UPDATE: Actually, we don't need this, we can use normal HashSet for
+        // uniqueness of Array, Collections
+
+        var hashSet = new HashSet<ArrayList<Integer>>();
+
+        var l1 = new ArrayList<Integer>();
+        l1.add(1);
+        l1.add(2);
+        hashSet.add(l1);
+
+        var l2 = new ArrayList<Integer>();
+        l2.add(1);
+        l2.add(2);
+        hashSet.add(l2);
+
+        System.out.println(hashSet);
+
+        var list = new ArrayList<ArrayList<Integer>>();
+        list.addAll(hashSet);
+
+        // UPDATE: TREESET NOT REQUIRED
         // We can use TreeSet to compare arrays in Set
         TreeSet<int[]> set = new TreeSet<>((a, b) -> Arrays.compare(a, b));
 
--- src/main.java
+++ src/main.java
@@ -4,30 +4,23 @@ import java.util.*;
 class Main {
 
     public static void main(String[] args) {
-        // Only works for Arrays
-        int[] array = new int[] { 1, 2, 3 };
-        System.out.println(Arrays.toString(Arrays.copyOf(array, 1)));
-        System.out.println(Arrays.toString(Arrays.copyOfRange(array, 1, 3)));
 
-        // Collection Slicing. ^^^ This does not create a copy unlike Arrays^^^^
-        // Can be used for LinkedList, ArrayList, etc.
-        List<Integer> collections = new ArrayList<>();
-        collections.add(1);
-        collections.add(2);
-        collections.add(3);
-        var slice = collections.subList(0, 2);
-        System.out.println(slice); // [1, 2]
-        slice.add(4); // This will change the original collection
-        System.out.println(slice); // [1, 2, 4]
-        System.out.println(collections); // [1,2, 4, 3]
-        // Never add value in slice. It'll be confusing. So just create a new collection
-        // and mutate it instead.
+        var hashSet = new HashSet<ArrayList<Integer>>();
 
-        // To create a copy from a collection, you need to create a new collection from
-        // old one.
-        List<Integer> collectionCopy = new ArrayList<>(collections);
-        System.out.println(collectionCopy);
+        var l1 = new ArrayList<Integer>();
+        l1.add(1);
+        l1.add(2);
+        hashSet.add(l1);
 
+        var l2 = new ArrayList<Integer>();
+        l2.add(1);
+        l2.add(2);
+        hashSet.add(l2);
+
+        System.out.println(hashSet);
+
+        var list = new ArrayList<ArrayList<Integer>>();
+        list.addAll(hashSet);
     }
 
 }
