4e326e440e294c1baf029e32fba5c791b97f5ec2
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-09-02 13:32:43 +0545

    Integer overflow

--- src/IntegerOverflow.java
+++ src/IntegerOverflow.java
@@ -2,7 +2,18 @@ public class IntegerOverflow {
     private static final int MOD = (int) 1e9 + 7;;
 
     public static void main(String[] args) {
+        // Method 1: Using long instead of int
         int[] values = { 1000000000, 1000000000, 1000000000 };
+        long sumLong = 0;
+
+        for (int value : values) {
+            sumLong += value;
+        }
+
+        System.out.println("Long: Sum that won't overflow for int: " + (int) (sumLong % MOD)); // Within Bound
+        System.out.println("Long: Sum that won't overflow for long: " + sumLong); // Won't overflow for long
+        System.out.println("Long: Sum that overflew for int: " + (int) sumLong); // But will overflow for int
+
         int sumWithModule = 0;
         int sum = 0;
 
@@ -11,8 +22,8 @@ public class IntegerOverflow {
             sum = sum + value;
         }
 
-        System.out.println("Sum with modulo is: " + sumWithModule); // Within Bound
-        System.out.println("Sum that overflew: " + sum); // Will overflow
+        System.out.println("Int: Sum that won't overflow for int: " + sumWithModule); // Within Bound
+        System.out.println("Int: Sum that overflew for int: " + sum); // Will overflow
 
     }
 }
