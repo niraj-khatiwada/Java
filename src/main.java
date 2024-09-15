46ab13f423b8e622bea4618618e962d074bc0f15
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-09-15 21:04:13 +0545

    Linked HashSet and Linked Hash Map

--- src/main.java
+++ src/main.java
@@ -2,25 +2,37 @@
 import java.util.*;
 
 class Main {
-    public static class Node {
-        public int value;
-
-        public Node(int value) {
-            this.value = value;
-        }
-    }
 
     public static void main(String[] args) {
-        Main.works();
+        // LinkedHashSet = LinkedList + HashSet
+        // With LinkedHashSet, the order to which the element is added is always
+        // guaranteed to be the same (like LinkedList) and there would be no duplicates
+        // (like HashSet).
+        var linkedHashSet = new LinkedHashSet<String>();
+        linkedHashSet.add("Hello");
+        linkedHashSet.add("World");
+        linkedHashSet.add("unknown");
+        linkedHashSet.add("Hello");
+        linkedHashSet.add("World");
 
-    }
+        linkedHashSet.remove("unknown");
 
-    public void works() {
-        var set = new HashSet<Node>();
-        var node1 = new Node(1);
-        var node2 = new Node(2);
-        set.add(node1);
-        set.add(node2);
+        System.out.println(linkedHashSet);
+
+        // LinkedHashMap = LinkedList + HashMap
+        // With LinkedHashMap, the order to which the entry is added is always
+        // guaranteed to be the same (like LinkedList) and there would be no duplicate
+        // entry (like HashMap).
+        var linkedHashMap = new LinkedHashMap<String, String>();
+        linkedHashMap.put("name", "Hello");
+        linkedHashMap.put("name", "World");
+        linkedHashMap.put("unknown", "<>");
+        linkedHashMap.put("age", "20");
+
+        linkedHashMap.remove("unknown");
+
+        System.out.println(linkedHashMap);
+
     }
 
 }
