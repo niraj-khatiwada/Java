894f5cb20bf38e733360abbd612e0f73e3e65eed
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-09-27 22:54:13 +0545

    Tree Map

--- src/main.java
+++ src/main.java
@@ -4,14 +4,13 @@ import java.util.*;
 class Main {
 
     public static void main(String[] args) {
-        var s = new Comparators();
-        s.doubleSorting();
 
-        // Sorting a sub-array
-        var fullArray = new int[] { 5, 3, 2, 1, 6, 0 };
-        Arrays.sort(fullArray, 0, 4); // Only sort starting from 0 to 3rd i.e [5, 3, 2, 1]. The end index is
-                                      // exclusive.
-        System.out.println(Arrays.toString(fullArray));
+        var map = new TreeMap<Integer, Integer>();
+        map.put(10, 20);
+        map.put(20, 30);
+        map.put(30, 40);
+        System.out.println(map.floorKey(25));
+        System.out.println(map.ceilingKey(25));
     }
 
 }
